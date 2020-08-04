ff7f189e14b2821b9b2a0320109f0c25456245f6
van thinh <nguyenthinhit996@gmail.com>
2020-08-04 20:12:19 +0700

    registry location user intime registry

--- app/src/main/java/com/example/truelove/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/truelove/activities/RegistrationActivity.java
@@ -2,11 +2,18 @@ package com.example.truelove.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,13 +25,19 @@ import android.widget.Toast;
 
 import com.example.truelove.R;
 import com.example.truelove.custom_class.User;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.compat.GeoDataClient;
+import com.google.android.libraries.places.compat.PlaceDetectionClient;
+import com.google.android.libraries.places.compat.Places;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,6 +46,16 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     private Button btnRegister;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    private boolean locationPermissionGranted;
+    static public final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    private GeoDataClient mGeoDataClient;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    private Double latitude;
+    private Double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +65,26 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         mapping();
         btnRegister.setOnClickListener(this);
         registration();
+
+
+        //==========================================================================================
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this);
+
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // set permission
+        getLocationPermission();
+        // get device location
+        getDeviceLocation();
+
+
+
     }
 
     @Override
@@ -75,9 +118,10 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
     private void personalUI() {
         getSupportActionBar().hide();
+        getSupportActionBar().hide();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
             w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             w.setStatusBarColor(Color.parseColor("#FB6667"));
@@ -135,6 +179,14 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
                                     user.setPhone("default");
                                     user.setSex(finalSex);
                                     user.setEmail(email);
+                                    if(latitude!=null&&longitude!=null){
+                                        user.setLatitude(latitude);
+                                        user.setLongitude(longitude);
+                                    }else{
+                                        user.setLatitude(37.42);
+                                        user.setLongitude(-122.08);
+                                    }
+
                                     System.out.println(user.toString());
                                     currentReference.setValue(user);
                                     Toast.makeText(RegistrationActivity.this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
@@ -148,4 +200,71 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
                 break;
         }
     }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            Location lastKnownLocation = task.getResult();
+
+                            if (lastKnownLocation != null) {
+                                double lat = lastKnownLocation.getLatitude();
+                                double longi = lastKnownLocation.getLongitude();
+                                Toast.makeText(RegistrationActivity.this, "LATITUDE =" + lat + "LONGI =" + longi , Toast.LENGTH_SHORT).show();
+                                latitude=lat;
+                                longitude=longi;
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
 }
