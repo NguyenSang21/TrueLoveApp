55dfdca1f379b16a0f1ca9b50c1d9991b4e89c06
nguyen sang <nguyentansansg1996@gmail.com>
2020-08-27 21:59:31 +0700

    logout

--- app/src/main/java/com/example/truelove/activities/SettingActivity.java
+++ app/src/main/java/com/example/truelove/activities/SettingActivity.java
@@ -3,14 +3,17 @@ package com.example.truelove.activities;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.truelove.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
 
 public class SettingActivity extends AppCompatActivity {
@@ -19,13 +22,14 @@ public class SettingActivity extends AppCompatActivity {
     private int km;
     private int minOld;
     private int maxOld;
+    private FirebaseAuth mAuth;
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
+        mAuth = FirebaseAuth.getInstance();
         //setup km setting
         seekBarKM = (SeekBar) findViewById(R.id.seekBarKM);
         txtKM = (TextView) findViewById(R.id.txtKM);
@@ -64,5 +68,13 @@ public class SettingActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void logoutUser (View view) {
+        mAuth.signOut();
+        Intent intent = new Intent(SettingActivity.this, ChooseLoginRegistrationActivity.class);
+        startActivity(intent);
+        finish();
+        return;
     }
 }
